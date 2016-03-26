9f32cec715e54210f45cab5303de993a66b09eb7
Hai Tang <402696415@qq.com>
2016-03-26 14:36:25 -0400

    Mar 26

---
+++ namesController.js
@@ -0,0 +1,7 @@
+angular.module('myApp2',[]).controller('namesCtrl',function($scope){
+	$scope.names = [
+		{name:'Shujie',country:'China'},
+		{name:'Helen',country:'China'},
+		{name:'Stellar',country:'Unknown'},
+		{name:'Nene',country:'Japan'}];
+});
