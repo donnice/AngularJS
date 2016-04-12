0547e51b530bc87f82c29fec3c84cbcd90a65ad8
Hai Tang <402696415@qq.com>
2016-04-12 19:08:21 -0400

    Apr 12th

--- ch7/script3.js
+++ ch7/script3.js
@@ -1,17 +1,18 @@
-function Controller($scope) {
-  $scope.master= {};
+angular.module('myApp',[])
+.controller('Controller',function($scope){
+  $scope.master = {};
 
-  $scope.update = function(user) {
-    $scope.master= angular.copy(user);
+  $scope.update = function(user){
+    $scope.master = angular.copy(user);
   };
 
-  $scope.reset = function() {
+  $scope.reset = function(){
     $scope.user = angular.copy($scope.master);
   };
 
-  $scope.isUnchanged = function(user) {
-    return angular.equals(user, $scope.master);
+  $scope.isUnchanged = function(user){
+    return angular.equals(user,$scope.master);
   };
 
   $scope.reset();
-}
+})
