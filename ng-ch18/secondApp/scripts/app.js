1f29fca06d237c2ad9ec3ca7d4e99e8f8ae83b26
Hai Tang <402696415@qq.com>
2016-04-25 15:01:39 -0400

    Apr 25th

--- ng-ch18/secondApp/scripts/app.js
+++ ng-ch18/secondApp/scripts/app.js
@@ -3,5 +3,20 @@ angular.module('myApp',[
 	'myApp.services',
 	'myApp.directives'])
 .config(function($routeProvider){
-	
-})
+	$routeProvider
+		.when('/',{
+			controller:'MainController',
+			templateUrl:'templates/main.html'
+		})
+		.otherwise({
+			redirectTo:'/'
+		});
+});
+
+window.onLoadCallBack = function(){
+	angular.element(document).ready(function(){
+		gapi.client.load('outh2','v2',function(){
+			angular.bootstrap(document,['myApp']);
+		});
+	});
+}
