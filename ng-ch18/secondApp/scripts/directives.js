c170f5344e53d7f3c32ba7e9f8df193cc51ffe2d
Hai Tang <402696415@qq.com>
2016-04-25 21:28:11 -0400

    Web Service

--- ng-ch18/secondApp/scripts/directives.js
+++ ng-ch18/secondApp/scripts/directives.js
@@ -37,7 +37,7 @@ angular.module('myApp.directives',[])
 				var po = document.createElement('script');
 			po.type = 'text/javascript';
 			po.async = true;
-			po.src = 'https://apis.google.com/\js/client:plusone.js?onload=onLoadCallback';
+			po.src = 'https://apis.google.com/js/client:plusone.js';
 			var s = document.getElementsByTagName('script')[0];
 			s.parentNode.insertBefore(po,s);
 			})();
